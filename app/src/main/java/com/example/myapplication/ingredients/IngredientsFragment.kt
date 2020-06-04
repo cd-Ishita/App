a5bd97317df155bde57172de1ef8248bbb076a22
cd-Ishita <ishitagupta.hyd@gmail.com>
2020-06-04 13:09:09 +0530

    icon

--- app/src/main/java/com/example/myapplication/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/example/myapplication/ingredients/IngredientsFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.myapplication.databinding.FragmentIngredientsBinding
+import kotlinx.android.synthetic.main.fragment_ingredients.*
 
 class IngredientsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +23,21 @@ class IngredientsFragment : Fragment() {
         val viewModelFactory = IngredientsViewModelFactory(myRecipe, application)
         binding.viewModel = ViewModelProviders.of(this, viewModelFactory).get(IngredientsViewModel::class.java)
 
+
+
+        binding.nextButton.setOnClickListener{view: View ->
+
+//            if(null != myRecipe.strInstructions) {
+//                val selection = myRecipe.strInstructions
+//            }
+
+            val selection: String = myRecipe.strInstructions ?: "Sorry"
+
+            val actionDetail = IngredientsFragmentDirections.actionIngredientsFragmentToInstructionsFragment()
+            actionDetail.setSelection(selection)
+            view.findNavController().navigate(actionDetail)
+            //view.findNavController().navigate(IngredientsFragmentDirections.actionIngredientsFragmentToInstructionsFragment(selection))
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/myapplication/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/myapplication/instructions/InstructionsFragment.kt
@@ -1,19 +1,14 @@
 package com.example.myapplication.instructions
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.Person.fromBundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.myapplication.R
-import com.example.myapplication.databinding.FragmentIngredientsBinding.inflate
 import com.example.myapplication.databinding.InstructionsFragmentBinding
-import com.example.myapplication.ingredients.IngredientsViewModel
-import com.example.myapplication.network.MyRecipe
+import com.example.myapplication.instructions.InstructionsFragmentArgs
+import com.example.myapplication.instructions.InstructionsFragmentArgs.fromBundle
 import kotlinx.android.synthetic.main.instructions_fragment.*
 
 class InstructionsFragment: Fragment() {
@@ -24,12 +19,21 @@ class InstructionsFragment: Fragment() {
     ): View? {
 
         val binding = InstructionsFragmentBinding.inflate(inflater)
-        val name = InstructionsFragmentArgs.fromBundle(arguments!!).selection
-        val recipe = name.strMeal
-        if(recipe != null)
-            the_recipe.text = recipe.toString()
-        else
-            the_recipe.text = "Sorry, not found"
+        //val name = InstructionsFragmentArgs.fromBundle(arguments!!).selection
+//        val recipe = name.strInstructions
+//        if(name != null)
+//            the_recipe.text = name
+//        else
+//            the_recipe.text = "Sorry, not found"
+
+        arguments?.let{
+            val safeArgs = InstructionsFragmentArgs.fromBundle(it)
+            val selection = safeArgs.selection
+            the_recipe.text = selection ?: "Sorry"
+        }
+
+
+
         binding.setLifecycleOwner(this)
 
 
